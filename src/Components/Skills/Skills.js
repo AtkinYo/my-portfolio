83166e9b16cb5dc1cb4aa49e7b780f7176abb573
AtkinYo <71451900+AtkinYo@users.noreply.github.com>
2021-10-07 00:05:22 -0700

    Tech carousel imported

---
+++ src/Components/Skills/Skills.js
@@ -0,0 +1,24 @@
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/swiper-bundle.min.css'
+import 'swiper/swiper.min.css'
+
+
+const Card = () => {
+  return (
+    <Swiper
+      spaceBetween={50}
+      slidesPerView={3}
+      onSlideChange={() => console.log('slide change')}
+      onSwiper={(swiper) => console.log(swiper)}
+    >
+      <SwiperSlide>Slide 1</SwiperSlide>
+      <SwiperSlide>Slide 2</SwiperSlide>
+      <SwiperSlide>Slide 3</SwiperSlide>
+      <SwiperSlide>Slide 4</SwiperSlide>
+      ...
+    </Swiper>
+  );
+};
+
+export default Card
--- src/Components/Styles/StyledProjects.js
+++ src/Components/Styles/StyledProjects.js
@@ -4,35 +4,31 @@ export const StyledProjects = styled.div`
   width: 100%;
   height: 100%;
 
-  h2{
+  h2 {
     font-weight: 800;
   }
 `;
 
 export const StyledProjectImg = styled.div`
   width: 100%;
-  height: 80vh;
   display: flex;
   flex-direction: column;
   justify-content: space-between;
 
-
-  .Projects{
+  .Projects {
     margin: 0 auto;
     text-align: center;
 
-    img{
+    img {
       cursor: pointer;
       max-width: 100%;
       height: auto;
+      margin-bottom: 6rem;
     }
   }
 
-  
   @media (min-width: 991px) {
     flex-direction: row;
-  justify-content: space-evenly;
-
+    justify-content: space-evenly;
   }
-
 `;
